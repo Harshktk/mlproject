1e55916c74b58abf9ec236eb36f2e95ff930361e
Harshktk <harshkatkade250@gmail.com>
2025-06-22 15:22:38 +0530

    Added data ingestion and working on data transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,26 @@
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+import os
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformation_object(self):
+        try:
+            pass
+        except:
+            pass        
